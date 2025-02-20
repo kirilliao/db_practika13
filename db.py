eaf0d21a79ec0bc1fae30a625ee1313d77d66c42
Kirill <kirill.wergssow.359539@gmail.com>
2025-02-20 10:30:01 +0300

    Вывод записей БД

--- db.py
+++ db.py
@@ -4,27 +4,33 @@ import sqlite3
 connection = sqlite3.connect('vkv_my_db.db')
 cursor = connection.cursor()
 
-### СОздание таблицы Users
-##cursor.execute('''
-##CREATE TABLE IF NOT EXISTS Users(
-##id INTEGER PRIMARY KEY,
-##username TEXT NOT NULL,
-##email TEXT NOT NULL,
-##age INTEGER
-##)
-##''')
+# СОздание таблицы Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
 
 ###Создание индекса
 ##cursor.execute('CREATE INDEX idx_email ON Users(email)')
 
-### Добавление нового пользователя
-##cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+# Добавление нового пользователя
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
 
 ### Обновление записей
 ##cursor.execute('UPDATE Users SET age = ? WHERE username = ?',(29, 'newuser'))
 
-# Удаление записей
-cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
+### Удаление записей
+##cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+
+for i in users:
+    print(i)
 
 connection.commit()
 connection.close()
