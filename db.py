6d654f78f1f53a11a952825ceae4eb80042ddea6
Kirill <kirill.wergssow.359539@gmail.com>
2025-02-20 11:50:52 +0300

    Использование ORDER BY

--- db.py
+++ db.py
@@ -18,7 +18,7 @@ age INTEGER
 ##cursor.execute('CREATE INDEX idx_email ON Users(email)')
 
 # Добавление нового пользователя
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 30))
 
 ### Обновление записей
 ##cursor.execute('UPDATE Users SET age = ? WHERE username = ?',(29, 'newuser'))
@@ -27,16 +27,24 @@ cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('ne
 ##cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
 
 ##cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
+
+### Получаем средний возраст пользователей для каждого возраста 
+##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
+##results = cursor.fetchall()
+##for i in results:
+##    print(i)
+##    
+### Фильтрация групп по среднему возросту больше 30
+##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
+##filter_results = cursor.fetchall()
+##for x in filter_results:
+##    print(x)
+
+# Выбираем и сортируем пользователей по возрасту по убыванию 
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
 results = cursor.fetchall()
-for i in results:
-    print(i)
-    
-# Фильтрация групп по среднему возросту больше 30
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
-filter_results = cursor.fetchall()
-for x in filter_results:
-    print(x)
+for row in results:
+    print(row)
 
 ##connection.commit()
 connection.close()
